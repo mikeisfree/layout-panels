502a8773533a5474647005baeea1161b9ce9055b
mikeisfree <mzaperty@gmail.com>
2024-10-10 14:50:05 +0200

    Add files via upload

---
+++ rooms/js/connectors.js
@@ -0,0 +1,10 @@
+// Perspective stairs
+
+var stairs = document.getElementById("stairs");
+
+function onWinScroll() {
+  var scrollVal = window.pageYOffset || document.body.scrollTop;
+  stairs.style.perspectiveOrigin = "10% " + (scrollVal / 3000) * 100 + "%";
+}
+
+window.addEventListener("scroll", onWinScroll);
